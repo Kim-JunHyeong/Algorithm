d1fe3571f73e5773374bcc23c1d905af852b45ff
Kim-JunHyeong <jhkim4685@gmail.com>
2018-10-04 01:15:26 +0900

    5-1 재귀의 기본

---
+++ DoIt/src/chapter05/recursive/EuclidGCD.java
@@ -0,0 +1,24 @@
+package chapter05.recursive;
+
+import java.util.Scanner;
+
+// 유클리드 호제법으로 최대공약수 구하기
+public class EuclidGCD {
+    // 정수 x, y의 최대공약수를 구하여 반환
+    static int gcd(int x, int y){
+        if(y==0)
+            return x;
+        else
+            return gcd(y, x % y);
+    }
+
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("두 정수의 최대 공약수 구하기");
+        System.out.println("정수 입력 : ");     int x = stdIn.nextInt();
+        System.out.println("정수 입력 : ");     int y = stdIn.nextInt();
+
+        System.out.println("최대 공약수 : " + gcd(x,y));
+    }
+}
---
+++ DoIt/src/chapter05/recursive/Question02.java
@@ -0,0 +1,31 @@
+package chapter05.recursive;
+
+import java.util.Scanner;
+
+// 재귀 호출 미사용 유클리드 호제법 작성
+public class Question02 {
+    public static void main(String[] args){
+        int gcd;
+        int x;
+        int y;
+        int tmp;
+
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("최대 공약수 구하기");
+        System.out.print("정수 입력 : ");   x = stdIn.nextInt();
+        System.out.print("정수 입력 : ");   y = stdIn.nextInt();
+
+        while(true){
+            if(y == 0){
+                gcd = x;
+                break;
+            }else{
+                tmp = x;
+                x = y;
+                y = tmp % y;
+            }
+        }   // while
+        System.out.println("최대 공약수 : "+ gcd);
+    }
+}
